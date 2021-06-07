f28382eaff4d5541ec7839f06150c587c64f2b76
Anime-Quatroctus <kanadetachibona@gmail.com>
2021-06-06 22:01:04 -0700

    Added multiply and divide functions along with tests.

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,7 @@
 """
 Tests for calculator.py
 """
-from calculator import add, subtract
+from calculator import add, divide, multiply, subtract
 
 
 class TestCalculator:
@@ -11,3 +11,9 @@ class TestCalculator:
 
     def test_sub(self):
         assert 5 == subtract(10, 5)
+
+    def test_mult(self):
+        assert 25 == multiply(5, 5)
+
+    def test_divide(self):
+        assert 3 == divide(21, 7)
